a14332807f443e28ea47e17812e2c9d5b100bb47
Ilya Dmitrichenko <errordeveloper@gmail.com>
2014-11-10 22:09:41 +0000

    Bootsrap with Yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name musicdayApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the musicdayApp
+ */
+angular.module('musicdayApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name musicdayApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the musicdayApp
+ */
+angular.module('musicdayApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
